095c2bf5461f13142e2ed1e7a43d827dcb7701fc
thanhnhanho <hothanhnhanhot@gmail.com>
2023-12-30 22:57:35 +0700

    add avatar update

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const usersController = require("../controllers/usersController");
+const multer = require("multer");
+const path = require("path");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
 const router = express.Router();
 
 router.post("/check", usersController.checkExistUser);
 router.post("/create", usersController.createNewUser);
 router.get("/", usersController.getAllUsers);
-router.patch("/update", usersController.updateUserInfo);
+router.patch("/update", upload.single("image"), usersController.updateUserInfo);
 router.get("/search", usersController.getUser);
-router.get("/logout", );
+router.get("/logout");
 
 module.exports = router;
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,29 +1,56 @@
 const Users = require("../database/Users.js");
+const fs = require("fs");
+const path = require("path");
 
 const checkExistUser = async (phoneNumber) => {
-    return await Users.checkExistUser(phoneNumber);
-}
+  return await Users.checkExistUser(phoneNumber);
+};
 
 const createNewUser = async (newUser) => {
-    await Users.createNewUser(newUser);
-}
+  await Users.createNewUser(newUser);
+};
 
 const getAllUsers = async () => {
-    return await Users.getAllUsers();
-}
+  return await Users.getAllUsers();
+};
 
 const getUser = async (fields, values) => {
-    return await Users.getUser(fields, values);
-}
+  return await Users.getUser(fields, values);
+};
 
-const updateUserInfo = async (fields, values, conditionFields, conditionValues) => {
-    await Users.updateUserInfo(fields, values, conditionFields, conditionValues);
-}
+const updateUserInfo = async (
+  fields,
+  values,
+  conditionFields,
+  conditionValues
+) => {
+  await Users.updateUserInfo(fields, values, conditionFields, conditionValues);
+};
+const updateImage = (newImagePath, username, callback) => {
+  const oldImagePath = path.join(
+    __dirname,
+    `../../public/images/${username}.jpg`
+  );
 
+  fs.unlink(oldImagePath, (err) => {
+    if (err) {
+      return callback({ message: "Lỗi không xóa avatar cũ" });
+    }
+
+    fs.rename(newImagePath, oldImagePath, (err) => {
+      if (err) {
+        return callback({ message: "Lỗi không cập nhật đc tên avatar" });
+      }
+
+      callback(null, "Cập nhật avatar thành công");
+    });
+  });
+};
 module.exports = {
-    checkExistUser,
-    createNewUser,
-    getAllUsers,
-    getUser,
-    updateUserInfo,
-}
+  checkExistUser,
+  createNewUser,
+  getAllUsers,
+  getUser,
+  updateUserInfo,
+  updateImage,
+};
